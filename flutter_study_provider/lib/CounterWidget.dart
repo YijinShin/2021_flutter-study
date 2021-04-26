287d558fbb54a4923596f7f68d44f55914c489a5
Yijin <49178058+o0heama0o@users.noreply.github.com>
2021-04-27 01:55:19 +0900

    Consumer 사용 추가

--- flutter_study_provider/lib/CounterWidget.dart
+++ flutter_study_provider/lib/CounterWidget.dart
@@ -8,10 +8,22 @@ class CounterWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body:  Center(
+        /*
+        //Provider.of 를 사용
         child: Text(
           Provider.of<CounterProvider>(context).count.toString(), //get으로 선언된 count를 부른거임. count는 int니까 stirng으로 바꿈.
           style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        */
 
+        //Consumer widget 을 사용
+        child: Consumer<CounterProvider>(
+          builder: (context, counter,_){
+            return Text(
+              "Total price: ${counter.count}",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            );
+          },
         ),
       ),
     );
